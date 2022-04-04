bd123e7b716dafb3a52e86bf87975dbcd4a28cc3
Seungho Hwang <tmdgh717@naver.com>
2022-04-04 21:52:52 +0900

    4 commit

--- challenge/lotto2/views.py
+++ challenge/lotto2/views.py
@@ -3,13 +3,12 @@ import random
 # Create your views here.
 
 def lotto2 (request) :
-
     num1 = request.GET.get('num1')
-    
+
     return render(request,'lotto2.html',{'game' : num1})
 
 def lottor (request) :
-
+    
     Lotto_no = list()
     for number in range(3) :
         number = random.sample(range(1,46),6)
