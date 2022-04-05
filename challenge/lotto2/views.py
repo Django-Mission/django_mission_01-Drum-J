4737720a3a691ac873501524b96e9d4080d812a0
Seungho Hwang <tmdgh717@naver.com>
2022-04-05 20:02:29 +0900

    5 commit

--- challenge/lotto2/views.py
+++ challenge/lotto2/views.py
@@ -8,12 +8,15 @@ def lotto2 (request) :
     return render(request,'lotto2.html',{'game' : num1})
 
 def lottor (request) :
-    
+    num1 = request.GET.get('num1')
     Lotto_no = list()
-    for number in range(3) :
+    for number in range(0,int(num1)) :
         number = random.sample(range(1,46),6)
         number.sort()
         Lotto_no.append(number)
         
         
-    return render(request,'lottoresult.html',{'Lotto_no' : Lotto_no})
+    return render(request,'lottoresult.html',{'Lotto_no' : Lotto_no, 'game' : num1})
+    # 값을 2개 보낼때는 {}안에 ,로 구분해서 넣어야 하는 듯!
+    # {'Lotto_no' : Lotto_no} ,{'game' : num1} 이렇게 넣었을때 자꾸 오류남
+    # 이것때문에 계속 안되서 속상....
